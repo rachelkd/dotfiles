00183e511494893fdc2f517db73de6b5dab4687b
rachelkd <rachel05deng@gmail.com>
2026-03-29 17:25:42 -0400

    sqlfluff conform for .sql files

--- .config/nvim/lua/plugins/conform.lua
+++ .config/nvim/lua/plugins/conform.lua
@@ -1,6 +1,9 @@
 return {
   "stevearc/conform.nvim",
   opts = {
+    formatters_by_ft = {
+      sql = { "sqlfluff" },
+    },
     formatters = {
       sqlfluff = {
         args = { "format", "--dialect", "postgres", "-" },
